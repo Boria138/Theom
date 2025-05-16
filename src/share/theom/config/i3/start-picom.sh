8bd4a7a2f80a8bebdd120aebec6d152c98bf36ee
Byson94 <byson94@outlook.com>
2025-05-16 19:32:14 +0530

    added shabang for start picom

--- src/share/theom/config/i3/start-picom.sh
+++ src/share/theom/config/i3/start-picom.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ "$(theom-config compositor | tr -d "[:space:]")" = "True" ]; then
     if [ "$(theom-config theme | tr -d '[:space:]')" = "light" ]; then
         picom --config /usr/share/theom/config/picom/picom-light.conf &
