6a95da25a77980b8cbc7101646a7ac21f1dcc882
Byson94 <byson94@outlook.com>
2025-05-19 09:08:51 +0530

    test with customisable polybar style

--- src/share/theom/config/polybar/launch.sh
+++ src/share/theom/config/polybar/launch.sh
@@ -8,12 +8,33 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar
+# Get theme (dark or light)
 theme=$(theom-config theme | tr -d "[:space:]")
-if [ "$theme" = "dark" ]; then
-    polybar -q main -c "$DIR"/dark/config.ini &
-elif [ "$theme" = "light" ]; then
-    polybar -q main -c "$DIR"/light/config.ini &
+
+# Get layout (float or stuck)
+layout=$(theom-config polybar-layout | tr -d "[:space:]")
+
+# Determine layout config files
+LAYOUT_DIR="$DIR/modules"
+LAYOUT_TARGET="$LAYOUT_DIR/layout.conf"
+LAYOUT_FLOAT="$LAYOUT_DIR/layout.float.conf"
+LAYOUT_STUCK="$LAYOUT_DIR/layout.stuck.conf"
+
+# Copy the appropriate layout config
+if [ "$layout" = "float" ]; then
+    cp "$LAYOUT_FLOAT" "$LAYOUT_TARGET"
+elif [ "$layout" = "stuck" ]; then
+    cp "$LAYOUT_STUCK" "$LAYOUT_TARGET"
 else
-    polybar -q main -c "$DIR"/dark/config.ini &
+    # fallback: default to float layout if unknown value
+    cp "$LAYOUT_FLOAT" "$LAYOUT_TARGET"
+fi
+
+# Launch Polybar with correct theme config
+if [ "$theme" = "dark" ]; then
+    polybar -q main -c "$DIR/dark/config.ini" &
+elif [ "$theme" = "light" ]; then
+    polybar -q main -c "$DIR/light/config.ini" &
+else
+    polybar -q main -c "$DIR/dark/config.ini" &
 fi
