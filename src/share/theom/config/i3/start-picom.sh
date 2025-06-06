27a329a165d621c2c11959125fd7cb62b4c51977
Byson94 <byson94@outlook.com>
2025-06-06 15:32:35 +0530

    added config to theom compatible across vm and hardware

--- src/share/theom/config/i3/start-picom.sh
+++ src/share/theom/config/i3/start-picom.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-if [ "$(theom-config features.compositing | tr -d "[:space:]")" = "True" ]; then
-    if [ "$(theom-config theme | tr -d '[:space:]')" = "light" ]; then
-        picom --config /usr/share/theom/config/picom/picom-light.conf &
-    else
-        picom --config /usr/share/theom/config/picom/picom-dark.conf &
+enable_compositing=$(theom-config compositor.compositing | tr -d "[:space:]")
+compositing_mode=$(theom-config compositor.compositing_mode | tr -d "[:space:]")
+theom_theme=$(theom-config appearance.theme | tr -d '[:space:]')
+
+if [ "$enable_compositing" = "True" ]; then
+    if [ "$compositing_mode" = "compatibility" ]; then
+        if [ "$theom_theme" = "light" ]; then
+            picom --config /usr/share/theom/config/picom/compatibility/picom-light.conf &
+        else
+            picom --config /usr/share/theom/config/picom/compatibility/picom-dark.conf &
+        fi
+    elif [ "$compositing_mode" = "performance" ]; then
+        if [ "$theom_theme" = "light" ]; then
+            picom --config /usr/share/theom/config/picom/performance/picom-light.conf &
+        else
+            picom --config /usr/share/theom/config/picom/performance/picom-dark.conf &
+        fi
+    #else
+    #    if [ "$theom_theme" = "light" ]; then
+    #        picom --config /usr/share/theom/config/picom/performance/picom-light.conf &
+    #    else
+    #        picom --config /usr/share/theom/config/picom/performance/picom-dark.conf &
+    #    fi # Well, no safety net if you did a typo ;)
     fi
 fi
