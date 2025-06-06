f6f8121cd6fca8ebb100e13cae1abc67761e67eb
Byson94 <byson94@outlook.com>
2025-06-06 20:15:44 +0530

    changed uptime widget

--- src/share/theom/config/widgets/uptime.lua
+++ src/share/theom/config/widgets/uptime.lua
@@ -5,9 +5,9 @@ conky.config = {
     own_window_transparent = true,
     own_window_hints = 'undecorated,below,sticky,skip_taskbar,skip_pager',
     double_buffer = true,
-    alignment = 'bottom_middle',
-    gap_x = 5,
-    gap_y = 340,
+    alignment = 'center',
+    gap_x = 0,
+    gap_y = -30,
     use_xft = true,
     font = 'FiraCode Nerd Font:bold:italic:size=12',
 };
